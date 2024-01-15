976efc727dac4cef1e43f3373e58193eac11a98f
RaulDiaz <rulhdiaz@gmail.com>
2024-01-15 12:14:54 -0600

    commit

--- server/src/Main.java
+++ server/src/Main.java
@@ -1,21 +1,49 @@
 import com.mongodb.client.MongoClient;
-import static spark.Spark.port;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        logger.info("Hello world!");
 
-        try(MongoClient mongoClient = MongoDBConnector.connect()){
-            System.out.println("Connected to MongoDB succesfully.");
+        // Connect to MongoDB
+        try (MongoClient mongoClient = MongoDBConnector.connect()) {
+            logger.info("Connected to MongoDB successfully.");
 
-            // Set up routes
-            Routes.setupRoutes();
+            // Start the HTTP server
+            startHttpServer();
 
-            // Start the Spark server on port 4567
-            port(4567);
+        } catch (Exception e) {
+            logger.error("Failed to connect to MongoDB: {}", e.getMessage());
+        }
+    }
 
-        }catch(Exception e){
-            System.err.println("Failed to connect to MongoDB: " + e.getMessage());
+    private static void startHttpServer() throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(4567), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+        logger.info("HTTP Server started on port 4567");
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "API is running!";
+            t.sendResponseHeaders(200, response.length());
+            try (OutputStream os = t.getResponseBody()) {
+                os.write(response.getBytes());
+            }
         }
     }
 }
--- server/src/MongoDBConnector.java
+++ server/src/MongoDBConnector.java
@@ -1,12 +1,12 @@
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-
+import com.mongodb.ConnectionString;
 
 public class MongoDBConnector {
-    private static final String CONNECTION_STRING = "mongodb://localhost:27017";
+    private static final ConnectionString connectionString =
+            new ConnectionString("mongodb://rulhdiaz:MFdzSGV2BqWkmctu@cluster0.mj44tp8.mongodb.net/webshop");
 
-
-    public static MongoClient connect(){
-        return MongoClients.create(CONNECTION_STRING);
+    public static MongoClient connect() {
+        return MongoClients.create(connectionString);
     }
 }
