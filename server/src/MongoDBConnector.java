a8c0d2a86a9629202ae8e5b39883b661ec31be59
RaulDiaz <rulhdiaz@gmail.com>
2024-01-16 08:02:22 -0600

    commit

--- server/src/MongoDBConnector.java
+++ server/src/MongoDBConnector.java
@@ -1,12 +1,24 @@
+import com.mongodb.ConnectionString;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import com.mongodb.ConnectionString;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class MongoDBConnector {
-    private static final ConnectionString connectionString =
-            new ConnectionString("mongodb://rulhdiaz:MFdzSGV2BqWkmctu@cluster0.mj44tp8.mongodb.net/webshop");
+    private static final ConnectionString connectionString = new ConnectionString(getMongoURI());
 
     public static MongoClient connect() {
         return MongoClients.create(connectionString);
+    }
+
+    private static String getMongoURI() {
+        try {
+            Dotenv dotenv = Dotenv.configure().load();
+            String mongoUri = dotenv.get("MONGO_URI");
+            System.out.println("Mongo URI from .env: " + mongoUri); // Print for debugging
+            return mongoUri;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
